08e2a1048a84a0f01d591e8a49b036c57fbde128
Sayantan-blip <309826334+Sayantan-blip@users.noreply.github.com>
2026-08-30 15:29:49 +0530

    Update guess game.py

--- guess game.py
+++ guess game.py
@@ -1,21 +1,22 @@
 import random
-jackpot = random.randint(1,100)
 
-guess = int(input("guess the number: "))
-counter=1
+while True:
+    jackpot = random.randint(1, 10)
 
-while guess != jackpot:
-    if guess < jackpot:
-        print("too low,try again")
+    for counter in range(1, 20):
+        guess = int(input("guess the number: "))
+        if guess < jackpot:
+            print("too low,try again")
+        elif guess > jackpot:
+            print("too high,try again")
+        else:
+            print("you guessed it!")
+            print("it took you", counter, "tries")
+            break
     else:
-        print("too high,try again")
-    guess = int(input("guess the number: "))
-    counter += 1
-    
-print("you guessed it!")
-print("it took you", counter,"tries")
-        
-        
+        print("you ran out of tries!")
 
-    
-            
+    again = str(input("do you want to go again? (y/n): "))
+    if again == "N" or again == "n":
+        print("aww i thought you were having fun, but ok")
+        break
